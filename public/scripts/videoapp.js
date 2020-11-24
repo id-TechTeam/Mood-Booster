4a29b52a79eb983d3633cd11afe3985c2bca1eab
Maya Buser De <maya.buser@gmail.com>
2020-11-24 14:28:56 -0500

    Added JS to make selection work and get another boost button (not sending anywhere yet)

--- public/scripts/videoapp.js
+++ public/scripts/videoapp.js
@@ -1,37 +1,39 @@
 
 //select the location of the picture and the button to trigger it
-const cats = document.querySelector('#cats');
-const button = document.querySelector('button');
+const pictures = document.querySelector('#picture');
+const btn = document.querySelector('#btn');
 
-// async function to add a new picture to the html in a div. Includes attribution text displayed with pictures
 
-const addNewCat = async () => {
-    //clear the screen from old cats
-    cats.innerHTML = "";
-    const cat = await getCat();
+//async function to add a new picture to the html in a div.Includes attribution text displayed with pictures
+
+const addNewPicture = async () => {
+    //clear the screen from old pictures
+    pictures.innerHTML = "";
+    const picture = await getPicture();
     const img = document.createElement("img");
     const credits = document.createElement('div');
 
-    const author = "https://unsplash.com/@" + cat.data.user.username + "?utm_source=Cute_Cat_Pics&utm_medium=referral";
-    credits.innerHTML = "Photo by " + "<a href = " + author + ">" + cat.data.user.name + "</a>" + " on " + "<a href = 'https://unsplash.com/?utm_source=Cute_Cat_Pics&utm_medium=referral'>" + "Unsplash" + "</a>";
+    const author = "https://unsplash.com/@" + picture.data.user.username + "?utm_source=Mood_Booster&utm_medium=referral";
+    credits.innerHTML = "Photo by " + "<a href = " + author + ">" + picture.data.user.name + "</a>" + " on " + "<a href = 'https://unsplash.com/?utm_source=Mood_Booster&utm_medium=referral'>" + "Unsplash" + "</a>";
     img.setAttribute('class', "rounded");
-    img.src = cat.data.urls.raw + "&fit=fill&fill=blur&w=400&h=400";
+    img.src = picture.data.urls.raw + "&fit=fill&fill=blur&w=400&h=400";
     credits.setAttribute('class', 'mt-3');
-    cats.append(img);
-    cats.append(credits);
+    pictures.append(img);
+    pictures.append(credits);
 
 }
 
-//async function to fetch the cat from the Unsplash API - needs sign up but is free up to 50 calls per hour. Can be customized more for size and query
-const getCat = async () => {
+//async function to fetch the cat from the Unsplash API - needs sign up but is free up to 50 calls per hour.Can be customized more for size and query
+const getPicture = async () => {
     try {
+        const selected = getSelected();
         const config = {
             headers: {
                 'Accept-Version': 'v1',
                 Authorization: PIC_API_KEY
             }
         }
-        const res = await axios.get('https://api.unsplash.com/photos/random?query=kitten', config)
+        const res = await axios.get('https://api.unsplash.com/photos/random?query=' + selected, config)
         return res;
     } catch (e) {
         return "NO CATS AVAILABLE! SORRY :("
@@ -39,13 +41,18 @@ const getCat = async () => {
 
 }
 
-addNewCat();
-//add listener to the button to trigger the adding function
-// cats.addEventListener('click', addNewCat);
-// button.addEventListener('click', addNewCat)
+// function to handle selection of different type of picture
+const getSelected = function () {
+    const selections = document.querySelectorAll('input[name="selection"]');
+    let selectedValue;
+    for (const selection of selections) {
+        if (selection.checked) {
+            selectedValue = selection.value;
+            break;
+        }
+    }
+    return selectedValue;
+};
 
-
-
-
-
-
+btn.addEventListener('click', addNewPicture);
+addNewPicture();
